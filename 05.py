4837a26aac68c85088e574cb91e3eb00c800d8c3
Anil <anilgulcik@gmail.com>
2022-11-28 14:06:15 +0300

    This is my fifth exercise on Python

---
+++ 05.py
@@ -0,0 +1,95 @@
+# my_tuple = (1, 4, 3, 57, 9, 20, 14)
+# my_list = list(my_tuple)
+# print(my_list, type(my_list))
+# my_list.append(2)
+# print(my_list)
+# my_list = tuple(my_list)
+# print(my_list)
+
+# state_capitals = {"Arkansas" : "Little Rock",
+#                 "Colorado" : "Denver",
+#                 "California" : "Sacramento",
+#                 "Georgia" : "Atlanta"
+
+
+# }
+
+# state_capitals["Virginia"] = "Richmond"
+# print(state_capitals)
+# print(state_capitals["Arkansas"])
+
+
+# mix_values = {"animal" : ("dog", "cat"),  # tuple
+#                 "planet" : ["Neptun", "Pluton", "Jupyter"],  # liste
+#                 "number" : 40,  # integer
+#                 "pi" : 3.14,  # float
+#                 "is_good" : False  # boolean
+# }
+
+# mix_keys = {22 : "number",
+#             3.14 : "float",
+#             True : "boolean",
+#             "key" : "string"
+# }
+
+# my_dictionary = dict(animal = ("dog", "cat"), planet = ["Neptun", "Pluton", "Saturn"], number = 55, pi = 3.14, is_goodv = False)
+# print(my_dictionary)
+# print(my_dictionary.items())
+# print(my_dictionary.keys())
+# print(my_dictionary.values())
+# mix_values.update(is_bad = True)
+# print(my_dictionary)
+# print(mix_values)
+# del mix_values["planet"]
+# print(mix_values)
+
+# school_records={
+# 	'personal_info':
+# 		{'kid':{'tom':{'class':'intermediate', 'age':10},
+# 			'sue':{'class':'elemantary', 'age':8}
+# 			},
+# 		'teen':{'joseph':{'class':'college', 'age':19},
+# 			'marry':{'class':'high school', 'age':16}
+# 			},
+# 		},
+# }
+
+# school_records={
+# 	'personal_info':
+# 		{'kid':{'tom':{'class':'intermediate', 'age':10},
+# 			'sue':{'class':'elemantary', 'age':8}
+# 			},
+# 		'teen':{'joseph':{'class':'college', 'age':19},
+# 			'marry':{'class':'high school', 'age':16}
+# 			}
+# 		},
+#     'grades_info':
+# 		{'kid':{'tom':{'math':88, 'speech':69},
+# 			'sue':{'math':90, 'speech':81}
+
+# 			},
+# 		'teen':{'joseph':{'coding':80, 'math':89},
+# 			'marry':{'coding':70, 'math':96}
+# 			}
+# 		}
+# }
+# print(school_records["grades_info"]["kid"]["sue"]["speech"])
+
+
+
+# flower_list = ["rose", "orchid", "cactus", "violet", "ginger", "rose", "orchid", "tulip", "tulip"]
+# flower_list = set(flower_list)
+# print(flower_list)
+
+# a = set("ankara")
+# b = set("istanbul")
+# c = set("izmir")
+# print(a, b, sep="\n")
+
+# print(b.difference(a, c))
+# print(b.difference(a))
+# print(a.union(b))
+# print(b.union(a))
+
+# print(a.intersection(b))
+
