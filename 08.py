aa13f580876c1bae8d983b7f7260f7d9b7fd1bc9
Anil <anilgulcik@gmail.com>
2022-12-06 23:03:26 +0300

    8th exercise

---
+++ 08.py
@@ -0,0 +1,88 @@
+# count = 0
+# while count < 5:
+#     print("Anil")
+#     count += 1
+# print("Alperen")
+
+# isim = "Anil"
+
+# for i in isim:
+#     print(i, end =" ")
+
+# times = int(input("How many times should I say 'I love you'"))
+
+# for i in range(times):
+#     print("I love you")
+
+# number = int(input("Enter a number: "))
+# for i in range(11):
+#     print(f"{number}x{i} = {number * i}")
+
+# b = set(range(0, 11))
+# print(b)
+
+# print(range(5))
+# print(*range(5))
+
+# print(*("seperate"))
+# print(*(range(10, 0, -2)))
+
+# numbers = [0, 1, 2, 3, 4, 5]
+# text = ["zero", "one", "two", "three", "four", "five"]
+# for x, y in zip(numbers, text):
+#     print(x, ":", y)
+
+# odd = []
+# even = []
+# for i in range(11):
+#     if i %2 == 0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+# print(even)
+# print(odd)
+
+# numbers = (11, 36, 33, 66, 89, 21, 32, 16, 10)
+# evens = []
+# odds = []
+# for i in numbers:
+#     if i %2 == 0:
+#         evens.append(i)
+#     else:
+#         odds.append(i)
+# print(evens)
+# print(odds)
+# print(f"The number of even numbers: {len(evens)}")
+# print(f"The number of odd numbers: {len(odds)}")
+
+# list = range(1, 10)
+# for i in list:
+#     print (str(i) * i)
+
+
+# sum = 0
+# list = range(1, 75)
+# for i in list:
+#     sum += i
+# print(sum)
+
+# who = ["I am", "You are"]
+# mood = ["happy", "confident"]
+# for i in who:
+#     for ii in mood:
+#         print (i, ii)
+
+
+# names = ["susan", "tom", "edward"] 
+# mood = ["happy", "sad"]
+
+# for i in names:
+#     for z in mood:
+#         print(i, "is", z)
+
+# for i in range(10):
+#     for k in range(10):
+#         print(f"{i}x{k}=", i*k)
+#     print("")
+
+
