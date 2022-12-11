beb8959f1d5293f0c4d31b200292dcc48eed0ffd
Anil <anilgulcik@gmail.com>
2022-12-11 23:00:19 +0300

    Nineth exercise on Python

---
+++ 09.py
@@ -0,0 +1,18 @@
+# fruits = ["banana", "mango", "pear", "apple", "kiwi", "grape"]
+# while True:
+#     try:
+#         selection = int(input("Please select your favourite fruit:  "))
+#         fav = fruits[selection-1]
+#         print(fav)
+#     except IndexError:
+#         print("Please enter a valid number. (1-6)")
+#     except Exception as e:
+#         print(f"This error has occurred: {e}")
+#     break
+
+
+
+# evens = [0, 2, 4, 6, 8]
+# odds = [1, 3, 5, 7, 9]
+# a = list(zip(evens, odds))
+# print(a)
