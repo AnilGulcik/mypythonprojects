58e95262fd48697bcd40230b7874aaf3e73c4c68
Anil <anilgulcik@gmail.com>
2022-12-17 09:16:17 +0300

    10 th exercise on Python

---
+++ 10.py
@@ -0,0 +1,73 @@
+# def ilk_fonksiyon(a,b):
+#     print((a ** 2) + (b ** 2))
+# ilk_fonksiyon(2,3)
+
+
+# a=5
+# b=10
+# ilk_fonksiyon(a,b)
+
+# def multiply(a,b):
+#     print(a * b)
+# multiply(3, 5)
+# multiply(-1, 2.5)
+# multiply("amazing ", 3)
+
+# def motto():
+#     print("Get up, Stand up, Don't give up the Fight")
+# motto()
+
+# def add(a, b):
+#     print(a + b)
+# add(3, 5)
+
+# def calculator(a, b):
+#     if calculator(a, b, "+"):
+#         print(a + b)
+#     elif calculator(a, b, "-"):
+#         print(a - b)
+#     elif calculator(a, b, "/"):
+#         print(a / b)
+#     else:
+#         print(a * b)
+
+# calculator(2, 3, "+")
+
+# def calculator(x, y, z):
+#     if (z == "+"):
+#         print(x + y)
+#     if (z == "-"):
+#         print(x - y)
+#     if (z == "*"):
+#         print(x * y)
+#     if (z == "/"):
+#         print(x / y)
+
+
+# calculator(88, 22, "+")
+# calculator(88, 22, "-")
+# calculator(88, 22, "*")
+# print(type(calculator(88, 22, "/")))
+
+# def absolute_value(val):
+#     return abs(val)
+# print(absolute_value(3.3))
+# print(absolute_value(-4))
+
+
+
+# def absolute_val(val):
+#     if val > 0:
+#         return val
+#     elif val < 0:
+#         return -1 * val
+#     elif val == 0:
+#         return "Zero is Zero"
+#     else:
+#         return "You have to enter a digit number!"
+
+# val = int(input("Please enter a digit number to see absolute value of it : "))
+# print(absolute_val(val))
+
+d = {"john":40, "peter":45} 
+d["john"]
