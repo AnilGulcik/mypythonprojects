00c5b57b14d489c24e4dbd57d9bdc4c612fdc39c
Anil <anilgulcik@gmail.com>
2023-03-23 14:44:59 +0300

    added

---
+++ session_4.py
@@ -0,0 +1,40 @@
+# text3 = ("         listen first        ")
+# print(text3.rstrip())
+
+# text = "tyou can learn almost everything in pre-classz"
+# print(text.lstrip("t").rstrip("z").upper())
+
+# country = ["USA", "New Zealand", "Finland", "Syria", "Turkey"]
+
+# country.append("Malatya")
+# print(country)
+# country.sort()
+# print(country)
+# country.sort(reverse=True)
+# print(country)
+
+# text = "2022's hard"
+
+# print(len(list(text)))
+
+# list = [1, 7, 5, 9]
+# print(list.sort())
+
+# list = [True, False, None]
+# print(len(list))
+
+# x = len(set('Listen to the voice of enlisted'))
+# print(x)
+
+
+
+# a={} 
+# a['a']=1 
+# a['b']=[2,3,4] 
+# print(a) 
+
+# def function1(var1=5, var2=7): 
+#     var2=9 
+#     var1=3 
+#     print (var1, " ", var2) 
+# function1(10,12)
